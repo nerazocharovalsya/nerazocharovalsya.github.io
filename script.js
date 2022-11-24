01b3e68fa3510ea751b2b3999e831e2d32568031
Budkov Dmitry <budima2003@mail.ru>
2022-11-24 19:02:54 +0300

    всё??

--- script.js
+++ script.js
@@ -409,3 +409,37 @@ class ItcSlider {
 document.addEventListener('DOMContentLoaded', () => {
     ItcSlider.createInstances();
 });
+
+const ratings = document.querySelectorAll('.rating')
+const ratingsContainer = document.querySelector('.ratings-container')
+const sendBtn = document.querySelector('#send')
+const panel = document.querySelector('#panel')
+let selectedRating = 'Satisfied'
+
+ratingsContainer.addEventListener('click', (e) => {
+    if(e.target.parentNode.classList.contains('rating')) {
+        removeActive()
+        e.target.parentNode.classList.add('active')
+        selectedRating = e.target.nextElementSibling.innerHTML
+    }
+    if(e.target.classList.contains('rating')) {
+        removeActive()
+        e.target.classList.add('active')
+        selectedRating = e.target.nextElementSibling.innerHTML
+    }
+
+})
+
+sendBtn.addEventListener('click', (e) => {
+    panel.innerHTML = `
+    
+        Спасибо за фидбек!
+        Это помогает нам быть лучше
+    `
+})
+
+function removeActive() {
+    for(let i = 0; i < ratings.length; i++) {
+        ratings[i].classList.remove('active')
+    }
+}
